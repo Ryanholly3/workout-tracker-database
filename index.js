d6f62a82cc85c84535d8e8230b3c584deebd3c3c
ryanholly3 <ryanholly3@gmail.com>
2018-11-09 11:25:51 -0700

    fixed error handling

--- index.js
+++ index.js
@@ -25,14 +25,15 @@ app.use('/workout_users/runs', runsPath)
 app.use(notFound)
 app.use(errorHandler)
 
-function notFound(req,res,next) {
-  res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl });
+function notFound(req, res, next) {
+    res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl });
 }
 
+// eslint-disable-next-line
 function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({error: err.message, stack, url: req.originalUrl })
+    console.error('ERROR', err);
+    const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined;
+    res.status(500).send({ error: err.message, stack, url: req.originalUrl });
 }
 
 
