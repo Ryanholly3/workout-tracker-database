e212a53320246d19a94b37115b8f4547289b9d5a
ryanholly3 <ryanholly3@gmail.com>
2018-11-09 10:45:48 -0700

    added get route in workout_users

--- routes/workout_users.js
+++ routes/workout_users.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/connection')
 
-
+router.get('/', (req, res) => {
+	knex('workout_users').then(workout_users => {
+			res.json({ workout_users })
+		})
+})
 
 
 module.exports = router
