21e005f2ce048693f3c77c3c3199a4af56aef965
ryanholly3 <ryanholly3@gmail.com>
2018-11-20 10:47:37 -0700

    improved user workout paths

--- routes/workout_users.js
+++ routes/workout_users.js
@@ -2,35 +2,34 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/connection')
 
+function getUsers(){
+	return knex('workout_users')
+		.select('*')
+}
 
-//Bug in JOIN!!!!!!!!!!!!!!!!!!!!
+function getSwimsForUser(user){
+	return knex('swim')
+		.select(knex.raw('swim.id as swim_id, swim.date, swim.distance, swim.difficulty, swim.notes'))
+		.innerJoin('workout_users', 'workout_users.id', 'swim.workout_users_id')
+		.whereIn('swim.workout_users_id', [user.id])
+}
+
+function getBikesForUser(user){
+	return knex('bike')
+		.select(knex.raw('bike.id as bike_id, bike.date, bike.distance, bike.difficulty, bike.notes'))
+		.innerJoin('workout_users', 'workout_users.id', 'bike.workout_users_id')
+		.whereIn('bike.workout_users_id', [user.id])
+}
+
+function getRunsForUser(user){
+	return knex('run')
+		.select(knex.raw('run.id as run_id, run.date, run.distance, run.difficulty, run.notes'))
+		.innerJoin('workout_users', 'workout_users.id', 'run.workout_users_id')
+		.whereIn('run.workout_users_id', [user.id])
+}
+
+
 router.get('/', (req, res) => {
-
-	function getUsers(){
-		return knex('workout_users')
-			.select('*')
-	}
-
-	function getSwimsForUser(user){
-		return knex('swim')
-			.select(knex.raw('swim.id as swim_id, swim.date, swim.distance, swim.difficulty, swim.notes'))
-			.innerJoin('workout_users', 'workout_users.id', 'swim.workout_users_id')
-			.whereIn('swim.workout_users_id', [user.id])
-	}
-
-	function getBikesForUser(user){
-		return knex('bike')
-			.select(knex.raw('bike.id as bike_id, bike.date, bike.distance, bike.difficulty, bike.notes'))
-			.innerJoin('workout_users', 'workout_users.id', 'bike.workout_users_id')
-			.whereIn('bike.workout_users_id', [user.id])
-	}
-
-	function getRunsForUser(user){
-		return knex('run')
-			.select(knex.raw('run.id as run_id, run.date, run.distance, run.difficulty, run.notes'))
-			.innerJoin('workout_users', 'workout_users.id', 'run.workout_users_id')
-			.whereIn('run.workout_users_id', [user.id])
-	}
 
 	function getUsersWithWorkouts(){
 		return getUsers()
@@ -51,60 +50,112 @@ router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   const intCheck = parseInt(id);
 
-  if(typeof intCheck === 'number'){
-    knex('workout_users')
-      .where('id', id)
-      .then(workout_user => {
-        if(!workout_user.length){
-          next()
-        } res.json({ workout_user: workout_user[0] })
-  		})
-  }
+	function getUser(){
+		return knex('workout_users')
+			.select('*')
+			.where('id', id)
+	}
+
+	function getUserWithWorkouts(){
+		return getUser()
+			.then(function(users){
+				return Promise.all(users.map(async (user)=> {
+						user.swims = await getSwimsForUser(user)
+						user.bikes = await getBikesForUser(user)
+						user.runs = await getRunsForUser(user)
+						return user
+					})
+				)
+			}).then(user => {
+					if(typeof intCheck === 'number' && user.length > 0){
+						res.json({ user })
+					} else {
+						next()
+					}
+				})
+	}
+	getUserWithWorkouts()
 })
 
 router.get('/:id/swims', (req, res, next) => {
   const id = req.params.id;
-  const intCheck = parseInt(id);
+	const intCheck = parseInt(id);
 
-  if(typeof intCheck === 'number'){
-    knex('swim')
-      .where('workout_users_id', id)
-      .then(workout_user_swims => {
-        if(!workout_user_swims.length){
-          next()
-        } res.json({ workout_user_swims })
-  		})
-  }
+	function getUser(){
+		return knex('workout_users')
+			.select('*')
+			.where('id', id)
+	}
+
+	function getUserWithSwims(){
+		return getUser()
+			.then(function(users){
+				return Promise.all(users.map(async (user)=> {
+						user.swims = await getSwimsForUser(user)
+						return user
+					})
+				)
+			}).then(user => {
+					if(typeof intCheck === 'number' && user.length > 0){
+						res.json({ user })
+					} else {
+						next()
+					}
+				})
+	}
+	getUserWithSwims()
 })
 
 router.get('/:id/bikes', (req, res, next) => {
   const id = req.params.id;
-  const intCheck = parseInt(id);
+	const intCheck = parseInt(id);
 
-  if(typeof intCheck === 'number'){
-    knex('bike')
-      .where('workout_users_id', id)
-      .then(workout_user_bikes => {
-        if(!workout_user_bikes.length){
-          next()
-        } res.json({ workout_user_bikes })
-  		})
-  }
+	function getUser(){
+		return knex('workout_users')
+			.select('*')
+			.where('id', id)
+	}
+
+	function getUserWithBikes(){
+		return getUser()
+			.then(function(users){
+				return Promise.all(users.map(async (user)=> {
+						user.bikes = await getBikesForUser(user)
+						return user
+					})
+				)
+			}).then(user => res.json({ user }))
+	}
+	getUserWithBikes()
 })
 
 router.get('/:id/runs', (req, res, next) => {
   const id = req.params.id;
-  const intCheck = parseInt(id);
+	const intCheck = parseInt(id);
 
-  if(typeof intCheck === 'number'){
-    knex('run')
-      .where('workout_users_id', id)
-      .then(workout_user_runs => {
-        if(!workout_user_runs.length){
-          next()
-        } res.json({ workout_user_runs })
-  		})
-  }
+	function getUser(){
+		return knex('workout_users')
+			.select('*')
+			.where('id', id)
+	}
+
+	function getUserWithRuns(){
+		return getUser()
+			.then(function(users){
+				return Promise.all(users.map(async (user)=> {
+						user.runs = await getRunsForUser(user)
+						return user
+					})
+				)
+			}).then(user => {
+					if(typeof intCheck === 'number' && user.length > 0){
+						res.json({ user })
+					} else {
+						next()
+					}
+				})
+	}
+	getUserWithRuns()
 })
 
 router.post('/', (req, res, next) => {
